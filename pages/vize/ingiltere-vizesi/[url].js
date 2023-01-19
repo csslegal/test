687b18e1e08c591ee03e0b9fa388ad013fc8eac1
CSS Legal <rplusmediaweb@gmail.com>
2023-01-19 15:00:29 +0300

    test

--- pages/vize/ingiltere-vizesi/[url].js
+++ pages/vize/ingiltere-vizesi/[url].js
@@ -1,7 +1,7 @@
 import Breadcrumb from "../../../components/Breadcrumb";
 import Layout from "../../../components/Sablon";
 import Post from "../../../components/Post";
-import SEO from "../../../components/SEO"; 
+import SEO from "../../../components/SEO";
 
 const siteWebApiUrl = process.env.NEXT_PUBLIC_WEB_API_URL;
 const siteWebApiId = process.env.NEXT_PUBLIC_WEB_API_ID;
@@ -13,14 +13,14 @@ export default function Url({ data }) {
       <Layout>
         <SEO meta={data} />
         <Breadcrumb title={data.title} />
-        <Post data={data} table="question" />
+        <Post data={data} table="article" />
       </Layout>
     </>
   );
 }
 
 export async function getStaticPaths() {
-  const res = await fetch(`${siteWebApiUrl}/api/v1/${siteWebApiId}/questions`);
+  const res = await fetch(`${siteWebApiUrl}/api/v1/${siteWebApiId}/articles`);
   const { data } = await res.json();
 
   const paths = data.map((post) => ({
@@ -31,7 +31,7 @@ export async function getStaticPaths() {
 
 export async function getStaticProps(context) {
   const res = await fetch(
-    `${siteWebApiUrl}/api/v1/question/${context.params.url}`
+    `${siteWebApiUrl}/api/v1/article/${context.params.url}`
   );
   const { data } = await res.json();
   return { props: { data }, revalidate: 1000 };
