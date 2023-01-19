3f07b2f2c4aa2169012cc2fff443ef2aa9b8c69d
CSS Legal <rplusmediaweb@gmail.com>
2023-01-19 15:39:38 +0300

    nav

--- components/Header.js
+++ components/Header.js
@@ -6,84 +6,83 @@ export default function Header() {
 
   return (
     <>
-     <header className="  py-2 mb-4 border-bottom fw-bold">
-      <nav
-        className="navbar navbar-expand-lg  rounded"
-        aria-label="Eleventh navbar example"
-      >
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">
-            Test Slogan
-          </a>
-          <button
-            className="navbar-toggler collapsed"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarsExample09"
-            aria-controls="navbarsExample09"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
+      <header className="  py-2 mb-4 border-bottom fw-bold">
+        <nav
+          className="navbar navbar-expand-lg  rounded"
+          aria-label="Eleventh navbar example"
+        >
+          <div className="container-fluid">
+            <a className="navbar-brand" href="/">
+              Test Slogan
+            </a>
+            <button
+              className="navbar-toggler collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarsExample09"
+              aria-controls="navbarsExample09"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
 
-          <div
-            className="navbar-collapse collapse d-lg-flex"
-            id="navbarsExample09"
-          >
-            <ul className="navbar-nav col-lg-12 justify-content-lg-end">
-              <li className="nav-item">
-                <Link
-                  href="/"
-                  className={
-                    router.pathname == "/" ? "active nav-link" : "nav-link"
-                  }
-                >
-                  Anasayfa
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  href="/vize/diger"
-                  className={
-                    router.pathname == "/vize/diger"
-                      ? "active nav-link"
-                      : "nav-link"
-                  }
-                >
-                  Diğer Türleri
-                </Link>
-              </li>
-              <Link
-                href="/vize/sss"
-                className={
-                  router.pathname == "/vize/sss"
-                    ? "active nav-link"
-                    : "nav-link"
-                }
-              >
-                S.S.S
-              </Link>
+            <div
+              className="navbar-collapse collapse d-lg-flex"
+              id="navbarsExample09"
+            >
+              <ul className="navbar-nav col-lg-12 justify-content-lg-end">
+                <li className="nav-item">
+                  <Link
+                    href="/"
+                    className={
+                      router.pathname == "/" ? "active nav-link" : "nav-link"
+                    }
+                  >
+                    Anasayfa
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    href="/vize/diger"
+                    className={
+                      router.pathname == "/vize/diger"
+                        ? "active nav-link"
+                        : "nav-link"
+                    }
+                  >
+                    Diğer Türleri
+                  </Link>
+                </li>
 
-              <li className="nav-item">
-                <Link
-                  href="/iletisim"
-                  className={
-                    router.pathname == "/iletisim"
-                      ? "active nav-link"
-                      : "nav-link"
-                  }
-                >
-                  İletişim
-                </Link>
-              </li>
-            </ul>
+                <li className="nav-item">
+                  <Link
+                    href="/vize/sss"
+                    className={
+                      router.pathname == "/vize/sss"
+                        ? "active nav-link"
+                        : "nav-link"
+                    }
+                  >
+                    S.S.S
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    href="/iletisim"
+                    className={
+                      router.pathname == "/iletisim"
+                        ? "active nav-link"
+                        : "nav-link"
+                    }
+                  >
+                    İletişim
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </nav>
-     
-
-
+        </nav>
       </header>
     </>
   );
