afaec7dbf227a4cbb692a6647ec6fe3989062b8d
CSS Legal <rplusmediaweb@gmail.com>
2023-01-24 17:57:18 +0300

    breadcrumd düzenleme

--- components/Breadcrumb.js
+++ components/Breadcrumb.js
@@ -5,7 +5,7 @@ const siteUrl = process.env.NEXT_PUBLIC_SITE_URL;
 export default function Breadcrumb({ title }) {
   return (
     <>
-      <nav aria-label="breadcrumb" className="mt-4">
+      <nav aria-label="breadcrumb" className="mt-2">
         <ol className="breadcrumb ">
           <li className="breadcrumb-item">
             <Link href={`${siteUrl}/`}>Anasayfa</Link>
