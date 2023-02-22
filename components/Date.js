f82c54a65e0d629a7e950b6e4d7e1377160ccb4a
CSS Legal <rplusmediaweb@gmail.com>
2023-02-22 14:15:02 +0300

    Tarih düzenlemesi

--- components/Date.js
+++ components/Date.js
@@ -3,5 +3,5 @@ import { parseISO, format } from "date-fns";
 export default function Date({ dateString = "2023" }) {
   const date = parseISO(dateString);
 
-  return <time dateTime={dateString}>{format(date, "yyyy-dd-MM")}</time>;
+  return <time dateTime={dateString}>{format(date, "yyyy-MM-dd")}</time>;
 }
